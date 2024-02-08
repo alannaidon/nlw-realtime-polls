25c340d6114ee114192ff6014bd8bd876b078f18
Alan Naidon <alan.naidon@zenvia.com>
2024-02-08 20:02:57 -0300

    Adding Websocket for Realtime events

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { randomUUID } from 'node:crypto'
 import { prisma } from "../../lib/prisma"
 import { z } from 'zod'
 import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -10,6 +11,8 @@ export async function voteOnPoll(app: FastifyInstance) {
     const voteOnPollParams = z.object({ pollId: z.string().uuid() })
 
 
+    console.log('pollId: ', request.params)
+    console.log('pollOptionId: ', request.body)
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
     let { sessionId } = request.cookies
@@ -28,18 +31,22 @@ export async function voteOnPoll(app: FastifyInstance) {
 
       // User can change the OPTION for the same Poll...
       if (userAlreadyVotedOnPoll && userAlreadyVotedOnPoll.pollOptionId !== pollOptionId) {
-        console.log('ENTROU NO IF')
         // If voting on different option
         await prisma.vote.delete({
           where: { id: userAlreadyVotedOnPoll.id }
         })
 
-        await redis.zincrby(pollId, -1, userAlreadyVotedOnPoll.pollOptionId)
+        const votes = await redis.zincrby(pollId, -1, userAlreadyVotedOnPoll.pollOptionId)
+
+        voting.publish(pollId, {
+          pollOptionId,
+          votes: Number(votes)
+        })
+
       } else if (userAlreadyVotedOnPoll) {
         return reply.status(400).send({ message: 'You already voted on this Poll!' })
       }
     }
-
 
     // Check if cookie already exists
     if (!sessionId) {
@@ -61,7 +68,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
-    await redis.zincrby(pollId, 1, pollOptionId)
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
 
     return reply.status(201).send()
   })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 import { createPoll } from './routes/create-polls'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 app.register(cookie, {
@@ -11,9 +13,12 @@ app.register(cookie, {
   parseOptions: {}
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP Server Running!')
